48b7e468c6aabe10887aa889a0fd980d5384a174
JyotikaW <JyotikaW@users.noreply.github.com>
2020-07-27 10:44:18 +0530

    Update provider.tf

--- Actimize-master/Customers/HSBC/provider.tf
+++ Actimize-master/Customers/HSBC/provider.tf
@@ -2,10 +2,3 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bucket-demo"
-    key    = "Customers/HSBC.tfstate"
-    region = "eu-west-2"
-  }
-}
